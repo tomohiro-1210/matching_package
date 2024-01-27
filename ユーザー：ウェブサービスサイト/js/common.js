c795eff309b31cb8f7875441489ece890f27026c
tomohiro_0225 <express22212270@gmail.com>
2024-01-27 17:25:55 +0900

    ーーー以下企業：ウェブサービスサイトページのコーディングーーー TOPページのコーディング完了 就活コラムページコーディング完了 就活マッチングサイトとはページコーディング完了

--- ユーザー：ウェブサービスサイト/js/common.js
+++ ユーザー：ウェブサービスサイト/js/common.js
@@ -11,3 +11,12 @@ $(".sp-menu__close").click(function () {
 $(".sp-menu__item").click(function () {
     $(".sp-menu").removeClass('open');
 });
+// TOPページスライダー
+$('.sp-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    speed: 1000,
+  });
