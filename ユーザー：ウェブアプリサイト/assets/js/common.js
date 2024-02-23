6f089acd248efb22d7d5b58dc21531325c17bc58
tomohiro_0225 <express22212270@gmail.com>
2024-02-23 12:50:56 +0900

    内定モーダル カレンダーモーダル 募集している仕事内容モーダル ダッシュボードモーダル 企業検索P オファーリストP コーディング完了。

--- ユーザー：ウェブアプリサイト/assets/js/common.js
+++ ユーザー：ウェブアプリサイト/assets/js/common.js
@@ -14,16 +14,26 @@ $(".l-header__humburgur").click(function () {
 });
 
 // 検索モーダル出現
-$(".p-search__window__button").click(function () {
+$('[data-dashboard]').click(function () {
     $(".p-modal-dashboard").addClass('appear');
 });
-// プロフィール充実依頼モーダル出現
-$(".irai-button").click(function () {
-    $(".p-modal-profile").addClass('appear');
+// 日程調整モーダル出現
+$('[data-calendar]').click(function () {
+    $(".p-modal-calendar").addClass('appear');
+});
+// 仕事内容モーダル出現
+$('[data-job]').click(function () {
+    $(".p-modal-job").addClass('appear');
+});
+// 内定モーダル出現
+$('[data-naitei]').click(function () {
+    $(".p-modal-naitei").addClass('appear');
 });
 // 検索モーダル閉じる
 $('[data-closeButton]').click(function () {
     $(".p-modal-dashboard").removeClass('appear');
-    $(".p-modal-profile").removeClass('appear');
+    $(".p-modal-calendar").removeClass('appear');
+    $(".p-modal-job").removeClass('appear');
+    $(".p-modal-naitei").removeClass('appear');
 });
 
